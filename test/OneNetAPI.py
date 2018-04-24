495940aa4a69d237850c82959910a58d79489ac7
slientsky <slientsky2007@gmail.com>
2018-04-24 18:22:50 +0800

    添加注释

--- test/OneNetAPI.py
+++ test/OneNetAPI.py
@@ -67,11 +67,14 @@ class onenet():
 		
 def main(argv):
 	m = argv[1:]
+	#检查是否有参数传入
 	if m == []:
 		print("Parameters Not exsit! Data won't post to OnetNet")
+	#检查配置文件是否存在
 	elif (os.path.isfile(argv[1]) == False):
 		print("file: "+ argv[1] + " not exist! Data won't post to OneNet")
 	else:
+		#捕获异常，检查是否对应配置文件中的参数
 		try:
 			_deviceid = BasicDef.get_pares_info(argv, 'deviceid')
 			print('_deviceid: ', _deviceid)
@@ -81,24 +84,29 @@ def main(argv):
 			print("--device=%s is not exsit!" % e)
 			return
 
-		
+		#检查没有问题，将配置文件中的_deviceid，_apikey 设置为全局可获取
 		BasicDef.set_device_id(_deviceid)
 		BasicDef.set_apikey(_apikey)
+		#初始化onenet对象
 		rPi = onenet()
+		#初始化计数器，用于控制循环次数
 		num = 0
 
 		while True:
 			if num < 1:
+				#构建RESTful data部分数据
 				rPi.set("Test1", random.randint(1, 100))
 				rPi.set("Test2", random.randint(1, 100))
-				print(rPi.dict)
+				# print(rPi.dict)
+				#上传数据到OneNet
 				r = rPi.post_data_flow()
 				num +=1
-				print(r.headers)
-				print('1',20 * '*')
-				print(r.text)
-				print('2',20 * '*')
+				# print(r.headers)
+				# print('1',20 * '*')
+				# print(r.text)
+				# print('2',20 * '*')
 				time.sleep(5)
+			#计数结束，退出循环
 			else:break
 
 if __name__ == "__main__":
